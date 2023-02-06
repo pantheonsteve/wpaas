468eb6e1fa1f33e69600defd79bdec8344800a38
Steve Bresnick <steve.bresnick@pantheon.io>
2023-02-06 17:20:36 -0500

    added index.php file to Edublock theme

---
+++ wp-content/themes/edublock/index.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * The main template file
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ *
+ * @package EduBlock
+ * @author  WPZOOM
+ * @license GNU General Public License v2 or later
+ * @link    https://www.wpzoom.com/
+ */
+
+get_header();
+
+if ( have_posts() ) {
+
+	// Load posts loop.
+	while ( have_posts() ) {
+		the_post();
+
+		get_template_part( 'template-parts/content/content', get_theme_mod( 'display_excerpt_or_full_post', 'excerpt' ) );
+	}
+
+	// Previous/next page navigation.
+	twenty_twenty_one_the_posts_navigation();
+
+} else {
+
+	// If no content, include the "No posts found" template.
+	get_template_part( 'template-parts/content/content-none' );
+
+}
+
+get_footer();
