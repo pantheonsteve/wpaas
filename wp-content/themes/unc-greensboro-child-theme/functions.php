e96a4d8743e06c3fd17163b8f7538649f6fb62a5
Steve Bresnick <steve.bresnick@pantheon.io>
2023-02-08 14:22:48 -0500

    rollback child theme

--- wp-content/themes/unc-greensboro-child-theme/functions.php
+++
@@ -1,6 +0,0 @@
-<?php 
-	 add_action( 'wp_enqueue_scripts', 'unc_greensboro_child_theme_enqueue_styles' );
-	 function unc_greensboro_child_theme_enqueue_styles() {
- 		  wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' ); 
- 		  } 
- ?>
